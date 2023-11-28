7516e664ee1e68c7c27bee457e68c2b6f2a47b53
EduardoCampos23 <eduardo.tdp09@gmail.com>
2023-11-28 18:56:18 -0300

    testesteste

--- Curso Python 700/z_Testes/teste_3.py
+++ Curso Python 700/z_Testes/teste_3.py
@@ -1,7 +1,8 @@
-cpf  = '74682489070'
+"""cpf  = '74682489070'
 nove_dig = cpf[:9]
 cont_regressivo = 10
 
 for numero in nove_dig:
     print(numero, cont_regressivo)
-    cont_regressivo -=1
+    cont_regressivo -=1"""
+"teste"
